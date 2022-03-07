83f5872eb4c9c6c6c24a71135d4e02465ef52d42
Aidan Cole <aidanjcole@gmail.com>
2022-03-06 20:23:53 -0500

    1.3.1 higher order functions example sum

--- sicp.rkt
+++ sicp.rkt
@@ -56,3 +56,33 @@
   (cond ((even? s) (search-for-primes (+ s 1) e))
         ((> s e))
         (else (timed-prime-test s) (search-for-primes (+ s 2) e))))
+
+;1.3.1
+
+(define (sum term a next b)
+  (if (> a b)
+      0
+      (+ (term a)
+         (sum term (next a) next b))))
+
+(define (cube x) (* x x x))
+
+(define (inc n) (+ n 1))
+(define (sum-cubes a b)
+  (sum cube a inc b))
+
+(define (identity x) x)
+(define (sum-integers a b)
+  (sum identity a inc b))
+
+(define (pi-sum a b)
+  (define (pi-term x)
+    (/ 1.0 (* x (+ x 2))))
+  (define (pi-next x)
+    (+ x 4))
+  (sum pi-term a pi-next b))
+
+(define (integral f a b dx)
+  (define (add-dx x) (+ x dx))
+  (* (sum f (+ a (/ dx 2.0)) add-dx b)
+     dx ))
