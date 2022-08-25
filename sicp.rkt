d2ece3a463fe4898b26de692a9b328a1773f4517
Aidan Cole <aidanjcole@gmail.com>
2022-08-25 16:39:29 -0400

    Added simpson's rule

--- sicp.rkt
+++ sicp.rkt
@@ -86,3 +86,13 @@
   (define (add-dx x) (+ x dx))
   (* (sum f (+ a (/ dx 2.0)) add-dx b)
      dx ))
+
+(define (simpson_rule f a b n)
+  (define h (/ (- b a) n))
+  (define (term k)
+    (cond ((= k 0) (f (+ a (* k h))))
+          ((= k n) (f (+ a (* k h))))
+          ((even? k) (* 2 (f (+ a (* k h)))))
+          (else (* 4 (f (+ a (* k h)))))))
+  (/ (* h (sum term 0 inc n)) 3))
+   
