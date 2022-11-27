529a278025f5b4a4c95327e20e6ac960c33b51f2
Aidan Cole <aidanjcole@gmail.com>
2022-11-26 21:32:42 -0500

    Excercise 1.33 b

--- sicp.rkt
+++ sicp.rkt
@@ -136,10 +136,26 @@
 (define (filtered-accumulate filter combiner null-value term a next b)
   (if (> a b)
       null-value
-      (if (filter (term a))
+      (if (filter a)
           (combiner (term a)
-                    (filtered-accumulate filter combiner null-value term (next a) next b)
-          (filtered-accumulate filter combiner null-value term (next a) next b)))))
+                    (filtered-accumulate filter combiner null-value term (next a) next b))
+          (filtered-accumulate filter combiner null-value term (next a) next b))))
 
+; ex 1.33 a
 (define (sum-prime-squares a b)
   (filtered-accumulate prime? + 0 square a inc b))
+
+; ex 1.33 b
+(define (gcd a b)
+  (if (= a 0)
+      b
+      (if (= b 0)
+          a
+          (gcd b (remainder a b)))))
+
+(define (coprime a b)
+  (= 1 (gcd a b)))
+
+(define (prod-rel-prime n)
+  (define (rel a) (coprime a n))
+  (filtered-accumulate rel * 1 identity 0 inc n))
