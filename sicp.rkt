64b8b215a37bf2e58834ec2933c8635ffffcb7a3
Aidan Cole <aidanjcole@gmail.com>
2023-02-28 16:32:36 -0500

    Excercise 1.36 fixed point printing

--- sicp.rkt
+++ sicp.rkt
@@ -217,5 +217,24 @@
 
 ; Excercise 1.35
 
-(define golden-ration (fixed-point (lambda (x) (+ 1 (/ 1.0 x))) 1))
-                
+(define golden-ratio (fixed-point (lambda (x) (+ 1 (/ 1.0 x))) 1))
+
+; Excercise 1.36
+
+(define (fixed-point-print f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (display guess)
+    (newline)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+           next
+           (try next))))
+    (try first-guess))
+
+; takes 35 guesses
+;(define x-to-x-is-1000 (fixed-point-print (lambda (x) (/ (log 1000) (log x))) 2))
+
+; takes 9 guesses
+;(define x-to-x-is-1000-damped (fixed-point-print (lambda (x) (/ (+ x (/ (log 1000) (log x))) 2)) 2))
