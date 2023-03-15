9b6e5b6b66378d4755127f80e6cbbcc313d4551f
Aidan Cole <aidanjcole@gmail.com>
2023-03-15 13:14:20 -0400

    Excercise 2.8

--- sicp.rkt
+++ sicp.rkt
@@ -519,3 +519,8 @@
 (define (upper-bound i)
   (cdr i))
 
+; Excercise 2.8
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
