a7158ce1fefe46f42ae5f8923e5f5762c50f04f2
Aidan Cole <aidanjcole@gmail.com>
2023-03-16 14:32:34 -0400

    Excercise 2.12

--- sicp.rkt
+++ sicp.rkt
@@ -582,3 +582,21 @@
         )
         )
   )
+
+; Excercise 2.12
+
+(define (make-center-width c w)
+  (make-interval (- c w) (+ c w)))
+(define (center i)
+  (/ (+ (lower-bound i) (upper-bound i)) 2))
+(define (width i)
+  (/ (- (upper-bound i) (lower-bound i)) 2))
+
+(define (make-center-percent c p)
+  (make-interval (* c (+ 1 (/ p 100))) (* c (- 1 (/ p 100)))))
+
+(define (percent i)
+  (* (/ (- (upper-bound i) (center i)) (center i)) 100))
+
+
+
