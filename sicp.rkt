79f773d713fad4d633e03e16dede0f436e1340de
Aidan Cole <aidanjcole@gmail.com>
2023-03-24 09:55:13 -0400

    Excercise 2.31

--- sicp.rkt
+++ sicp.rkt
@@ -782,3 +782,11 @@
              (square-tree-map sub-tree)
              (* sub-tree sub-tree)))
        tree))
+
+;Ex 2.31
+
+(define (tree-map f tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (f tree))
+        (else (cons (tree-map f (car tree))
+                    (tree-map f (cdr tree))))))
