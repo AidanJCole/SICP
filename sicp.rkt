6b888e33b809f8fea71c8ae46714a4235b3a9741
Aidan Cole <aidanjcole@gmail.com>
2023-03-28 16:42:55 -0400

    Section 2.2.3

--- sicp.rkt
+++ sicp.rkt
@@ -799,3 +799,45 @@
       (let ((rest (subsets (cdr s))))
         (append rest (map (lambda (r) (cons (car s) r)) rest)))))
 
+;Section 2.2.3
+
+(define (sum-odd-squares tree)
+  (cond ((null? tree) 0)
+        ((not (pair? tree))
+         (if (odd? tree) (square tree) 0))
+        (else (+ (sum-odd-squares (car tree))
+                 (sum-odd-squares (cdr tree))))))
+
+;(define (even-fibs n)
+;  (define (next k)
+;    (if (> k n)
+;        nil
+;        (let ((f (fib k)))
+;          (if (even? f)
+;              (cons f (next (+ k 1)))
+;              (next (+ k 1))))))
+;  (next 0))
+
+(define (filter predicate sequence)
+  (cond ((null? sequence) nil)
+        ((predicate (car sequence))
+         (cons (car sequence)
+               (filter predicate (cdr sequence))))
+        (else (filter predicate (cdr sequence)))))
+
+(define (accumulater op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulater op initial (cdr sequence)))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+      nil
+      (cons low (enumerate-interval (+ low 1) high))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
